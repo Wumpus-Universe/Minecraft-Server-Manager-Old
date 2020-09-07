9c3d137d6b948d700f92fc14f08147503bb9b955
Thrasilias <46424920+Thrasilias@users.noreply.github.com>
2020-09-07 12:48:00 +0200

    Update servers.js

--- app/servers.js
+++ app/servers.js
@@ -1,9 +1,9 @@
 // Here is where you "configure" your servers. The name should be a unique id and the value should be the name of the folder
 module.exports = {
-	single: "Single Player",
-	castle: "Castle",
-	experiments: "Experiments",
-	friendville: "Friendville",
-	house: "House",
-	sharn: "Sharn"
+        survival: "Survival",
+	lobby: "Lobby",
+	development01: "Development 1 (Thrasilias)",
+	development02: "Development 2 (Alexis)",
+	affiliate01: "Affiliate (Name)", 
+	affiliate02: "Affiliate (Name)"
 };
